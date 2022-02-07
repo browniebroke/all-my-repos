e0c07267bb7701b359e5ad1bcdaf70ebedd9c852
Bruno Alla <alla.brunoo@gmail.com>
2022-02-07 21:17:37 +0000

    feat: custom fix to remove pytest-srcpaths

---
+++ run_fix.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+import argparse
+
+from all_repos import autofix_lib
+from all_repos.grep import repos_matching
+
+
+def find_repos(config) -> set[str]:
+    return repos_matching(config, ("pytest-srcpaths", "--", "pyproject.toml"))
+
+
+def apply_fix():
+    autofix_lib.run("poetry", "remove", "-D", "pytest-srcpaths")
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    autofix_lib.add_fixer_args(parser)
+    args = parser.parse_args(argv)
+
+    repos, config, commit, autofix_settings = autofix_lib.from_cli(
+        args,
+        find_repos=find_repos,
+        msg="chore(deps): remove pytest-srcpaths",
+        branch_name="deps/remove-pytest-srcpaths",
+    )
+    print(repos)
+    autofix_lib.fix(
+        repos,
+        apply_fix=apply_fix,
+        config=config,
+        commit=commit,
+        autofix_settings=autofix_settings,
+    )
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
