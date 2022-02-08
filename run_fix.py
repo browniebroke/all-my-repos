fee459e273fb05642595f4751c773f583cad50ff
Bruno Alla <alla.brunoo@gmail.com>
2022-02-08 21:12:52 +0000

    feat: drop Python 3.6 everywhere

--- run_fix.py
+++ run_fix.py
@@ -8,20 +8,19 @@ from all_repos.grep import repos_matching
 
 
 def find_repos(config) -> set[str]:
-    repos = repos_matching(config, ("black =", "--", "pyproject.toml"))
+    repos = repos_matching(config, ('python = "^3.6"', "--", "pyproject.toml"))
     return repos
 
 
 def apply_fix():
     pyproject_toml = Path("pyproject.toml")
     content = pyproject_toml.read_text()
-    if 'black = {version = "^22.1"' in content or 'black = "^22.1"' in content:
+    if 'python = "^3.7"' in content:
         return
     content = content.replace(
-        'black = {version = "^21.9b0"',
-        'black = {version = "^22.1"'
+        'python = "^3.6"',
+        'python = "^3.7"'
     )
-    content = re.sub(r'black = ".+"', 'black = "^22.1"', content)
     pyproject_toml.write_text(content)
     autofix_lib.run("poetry", "lock")
 
@@ -34,8 +33,8 @@ def main(argv=None):
     repos, config, commit, autofix_settings = autofix_lib.from_cli(
         args,
         find_repos=find_repos,
-        msg="chore: upgrade to black stable",
-        branch_name="upgrade-black-stable",
+        msg="feat: drop Python 3.6",
+        branch_name="drop-python3.6",
     )
     autofix_lib.fix(
         repos,
