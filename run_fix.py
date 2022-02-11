18797423496c5a1eea1265924d04189d190f5ed1
Bruno Alla <alla.brunoo@gmail.com>
2022-02-11 15:59:17 +0000

    feat: drop patch versions

--- run_fix.py
+++ run_fix.py
@@ -4,6 +4,7 @@ import argparse
 import re
 from pathlib import Path
 
+import tomli
 from all_repos import autofix_lib
 from all_repos.grep import repos_matching
 
@@ -15,24 +16,44 @@ def find_repos(config) -> set[str]:
 
 
 def apply_fix():
-    pre_commit_config = Path(".pre-commit-config.yaml")
-    if not pre_commit_config.exists():
-        return
-    content = pre_commit_config.read_text()
-    if "repo: https://github.com/PyCQA/bandit" in content:
-        return
-
-    content += "\n".join(
-        [
-            "  - repo: https://github.com/PyCQA/bandit",
-            "    rev: 1.7.2",
-            "    hooks:",
-            "      - id: bandit",
-            "        args: [-x, tests]",
-            "",
-        ]
-    )
-    pre_commit_config.write_text(content)
+    pyproject_toml = Path("pyproject.toml")
+    content = pyproject_toml.read_text()
+    pyproject_config = tomli.loads(content)
+    deps = pyproject_config["tool"]["poetry"]["dependencies"]
+    dev_deps = pyproject_config["tool"]["poetry"]["dev-dependencies"]
+    all_deps = {**deps, **dev_deps}
+    for package, version_spec in all_deps.items():
+        if package == "python":
+            continue
+        if isinstance(version_spec, dict):
+            version = version_spec['version']
+            digits = version.split(".")
+            if digits[0].lstrip("^>=<") > "0" and len(digits) > 2:
+                # patch specified for non zero-ver package
+                updated_version = ".".join(digits[:2])
+                content = content.replace(
+                    ' = {version =',
+                    ' = { version =',
+                )
+                content = content.replace(
+                    'optional = true}',
+                    'optional = true }',
+                )
+                content = content.replace(
+                    package + ' = { version = "' + version + '"',
+                    package + ' = { version = "' + updated_version + '"',
+                )
+        else:
+            digits = version_spec.split(".")
+            if digits[0].lstrip("^>=<") > "0" and len(digits) > 2:
+                # patch specified for non zero-ver package
+                updated_version = ".".join(digits[:2])
+                content = content.replace(
+                    f'{package} = "{version_spec}"',
+                    f'{package} = "{updated_version}"',
+                )
+    pyproject_toml.write_text(content)
+    autofix_lib.run("poetry", "lock", "--no-update")
 
 
 def main(argv=None):
@@ -43,8 +64,8 @@ def main(argv=None):
     repos, config, commit, autofix_settings = autofix_lib.from_cli(
         args,
         find_repos=find_repos,
-        msg="chore: add bandit to pre-commit config",
-        branch_name="pre-commit-bandit-2",
+        msg="chore: drop patch version for all stable dependencies",
+        branch_name="drop-patch",
     )
     autofix_lib.fix(
         repos,
