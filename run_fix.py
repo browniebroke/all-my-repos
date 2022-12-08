9ae9fd5b9e489e99658ef59d1ab02461bffc2ffe
Bruno Alla <alla.brunoo@gmail.com>
2022-12-08 19:21:21 +0000

    Add Python 3.12 to the build

--- run_fix.py
+++ run_fix.py
@@ -15,9 +15,9 @@ def find_repos(config) -> set[str]:
     repos = repos_matching(
         config,
         (
-            "tool.poetry.urls",
+            f"python-version:",
             "--",
-            "pyproject.toml",
+            ".github/workflows/ci.yml",
         ),
     )
     print(repos)
@@ -25,28 +25,16 @@ def find_repos(config) -> set[str]:
 
 
 def apply_fix():
-    pyproject_toml = Path("pyproject.toml")
-    if "Twitter" in (content := pyproject_toml.read_text()):
+    ci_yml = Path(".github/workflows/ci.yml")
+    file_content = ci_yml.read_text()
+    if '- "3.12-dev"' in file_content:
         return
 
-    lines = content.splitlines()
-    in_section = False
-    out_lines = []
-    for line in lines:
-        if in_section and line == "":
-            out_lines.extend(
-                [
-                    '"Twitter" = "https://twitter.com/_BrunoAlla"',
-                    '"Mastodon" = "https://fosstodon.org/@browniebroke"',
-                ]
-            )
-            in_section = False
-        if line == "[tool.poetry.urls]":
-            in_section = True
-        out_lines.append(line)
-
-    out_lines.append("")
-    pyproject_toml.write_text("\n".join(out_lines))
+    file_content = file_content.replace(
+        '          - "3.11"',
+        '          - "3.11"\n          - "3.12-dev"',
+    )
+    ci_yml.write_text(file_content)
 
 
 def main(argv=None):
@@ -57,8 +45,8 @@ def main(argv=None):
     repos, config, commit, autofix_settings = autofix_lib.from_cli(
         args,
         find_repos=find_repos,
-        msg="chore: add links to social media on PyPI",
-        branch_name=f"chore/links-social-media-pypi",
+        msg=f"ci: officially support Python 3.12",
+        branch_name=f"feat/python3.12",
     )
     autofix_lib.fix(
         repos,
