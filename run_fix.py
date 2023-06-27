941b74b5ab0a5b58f9c23ea98261a06a12f03d46
Bruno Alla <alla.brunoo@gmail.com>
2023-06-27 13:31:07 +0100

    Update commitlint config

--- run_fix.py
+++ run_fix.py
@@ -6,14 +6,24 @@ from pathlib import Path
 from all_repos import autofix_lib
 from all_repos.grep import repos_matching
 
+CONTENT = """module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [0, "always", Infinity],
+    "body-max-line-length": [0, "always", Infinity],
+    "footer-max-line-length": [0, "always", Infinity],
+  },
+};
+"""
+
 
 def find_repos(config) -> set[str]:
     repos = repos_matching(
         config,
         (
-            "poetry",
+            "config-conventional",
             "--",
-            "pyproject.toml",
+            "commitlint.config.js",
         ),
     )
     print(repos)
@@ -21,48 +31,8 @@ def find_repos(config) -> set[str]:
 
 
 def apply_fix():
-    file_path = Path("pyproject.toml")
-    content = file_path.read_text()
-
-    if 'python = "^3.8"' in content:
-        return
-
-    content = content.replace('python = "^3.7"', 'python = "^3.8"')
-    file_path.write_text(content)
-
-    ci_yml = Path(".github/workflows/ci.yml")
-    if ci_yml.exists():
-        content = ci_yml.read_text()
-        content = content.replace('python-version:\n          - "3.7"\n', 'python-version:\n')
-        ci_yml.write_text(content)
-
-    pc_yml = Path(".pre-commit-config.yaml")
-    if pc_yml.exists():
-        content = pc_yml.read_text()
-        content = content.replace('--py37-plus', '--py38-plus')
-        pc_yml.write_text(content)
-
-    template_path = Path("project")
-    if template_path.exists():
-        file_path = template_path / "pyproject.toml.jinja"
-        content = file_path.read_text()
-        content = content.replace('python = "^3.7"', 'python = "^3.8"')
-        file_path.write_text(content)
-
-        ci_yml = template_path / ".github" / "workflows" / "ci.yml"
-        content = ci_yml.read_text()
-        content = content.replace('python-version:\n          - "3.7"\n', 'python-version:\n')
-        ci_yml.write_text(content)
-
-        pc_yml = template_path / ".pre-commit-config.yaml"
-        content = pc_yml.read_text()
-        content = content.replace('--py37-plus', '--py38-plus')
-        pc_yml.write_text(content)
-
-        readme = template_path / "README.md"
-        content = readme.read_text()
-        content = content.replace('Python 3.7+.', 'Python 3.8+.')
-        readme.write_text(content)
+    file_path = Path("commitlint.config.js")
+    file_path.write_text(CONTENT)
 
 
 def main(argv=None):
@@ -74,11 +44,9 @@ def main(argv=None):
         args,
         find_repos=find_repos,
         msg=(
-            "feat: drop support for Python 3.7\n\n"
-            "BREAKING CHANGE: Drop support for Python 3.7 as it reached EOL on June 27, 2023. "
-            "More infos: https://devguide.python.org/versions/"
+            "chore: update commitlint config to be more permissive"
         ),
-        branch_name=f"feat/drop-python-3.7",
+        branch_name=f"chore/commitlint-cfg",
     )
     autofix_lib.fix(
         repos,
