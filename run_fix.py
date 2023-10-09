c039a5b5a8a22e510a5a63066646f576e6c671bc
Bruno Alla <alla.brunoo@gmail.com>
2023-10-09 22:40:11 +0100

    Migrate PSR action to its new repo

--- run_fix.py
+++ run_fix.py
@@ -1,148 +1,32 @@
 from __future__ import annotations
 
 import argparse
-import re
 from pathlib import Path
 
 from all_repos import autofix_lib
 from all_repos.grep import repos_matching
 
 # Find repos that have this file...
-FILE_NAME = ".pre-commit-config.yaml"
+FILE_NAME = ".github/workflows/ci.yml"
 # ... and which content contains this string.
-FILE_CONTAINS = "https://github.com/PyCQA/flake8"
+FILE_CONTAINS = "relekang/python-semantic-release"
 # Git stuff
-GIT_COMMIT_MSG = "chore: switch linting to Ruff"
-GIT_BRANCH_NAME = "chore/lint-ruff"
-
-RUFF_PRECOMMIT_LINES = [
-    "  - repo: https://github.com/astral-sh/ruff-pre-commit",
-    "    rev: v0.0.286",
-    "    hooks:",
-    "      - id: ruff",
-    "        args: [--fix, --exit-non-zero-on-fix]",
-]
-RUFF_PYPROJECT_CONTENT = """
-[tool.ruff]
-target-version = "py38"
-line-length = 88
-ignore = [
-    "D203", # 1 blank line required before class docstring
-    "D212", # Multi-line docstring summary should start at the first line
-    "D100", # Missing docstring in public module
-    "D104", # Missing docstring in public package
-    "D107", # Missing docstring in `__init__`
-    "D401", # First line of docstring should be in imperative mood
-]
-select = [
-    "B",   # flake8-bugbear
-    "D",   # flake8-docstrings
-    "C4",  # flake8-comprehensions
-    "S",   # flake8-bandit
-    "F",   # pyflake
-    "E",   # pycodestyle
-    "W",   # pycodestyle
-    "UP",  # pyupgrade
-    "I",   # isort
-    "RUF", # ruff specific
-]
-
-[tool.ruff.per-file-ignores]
-"tests/**/*" = [
-    "D100",
-    "D101",
-    "D102",
-    "D103",
-    "D104",
-    "S101",
-]
-"""
+GIT_COMMIT_MSG = "chore: switch PSR action to new repo"
+GIT_BRANCH_NAME = "chore/psr-action"
 
 
 def apply_fix():
     """Apply fix to a matching repo."""
-    _update_pre_commit_config()
-    _update_pyproject_toml()
-    _remove_flake8_config()
-    print("all done")
-
-
-def _update_pre_commit_config():
-    pre_commit_yml = Path(".pre-commit-config.yaml")
-    content = pre_commit_yml.read_text()
-    if "https://github.com/astral-sh/ruff-pre-commit" in content:
+    ci_yml = Path(FILE_NAME)
+    content = ci_yml.read_text()
+    if "relekang/python-semantic-release" not in content:
         return
 
-    updated_lines = []
-    keep = True
-    for line in content.splitlines():
-        if line.startswith('exclude: "') and ".all-contributorsrc" not in line:
-            line = re.sub(r'"$', '|.all-contributorsrc"', line)
-
-        if "- repo:" in line:
-            # Reset to tru for each new repo
-            keep = True
-
-        if (
-            "https://github.com/asottile/pyupgrade" in line
-            or "https://github.com/PyCQA/isort" in line
-            or "https://github.com/PyCQA/flake8" in line
-            or "https://github.com/PyCQA/bandit" in line
-        ):
-            # Remove the above linters
-            keep = False
-
-        if "https://github.com/psf/black" in line:
-            # Insert new config before black
-            updated_lines.extend(RUFF_PRECOMMIT_LINES)
-
-        if keep:
-            updated_lines.append(line)
-
-    updated_lines.append("")
-    pre_commit_yml.write_text("\n".join(updated_lines))
-
-
-def _update_pyproject_toml():
-    pyproject_toml = Path("pyproject.toml")
-    content = pyproject_toml.read_text()
-    if "[tool.ruff]" in content:
-        return
-
-    updated_lines = []
-    for line in content.splitlines():
-        if line == "[tool.isort]":
-            ruff_lines = [ln for ln in RUFF_PYPROJECT_CONTENT.splitlines() if ln]
-            updated_lines.extend(ruff_lines)
-            if Path("setup.py").exists():
-                updated_lines.append('"setup.py" = ["D100"]')
-            if Path("conftest.py").exists():
-                updated_lines.append('"conftest.py" = ["D100"]')
-            if Path("docs/conf.py").exists():
-                updated_lines.append('"docs/conf.py" = ["D100"]')
-            updated_lines.append("")
-            updated_lines.append("[tool.ruff.isort]")
-            continue
-        if line == 'profile = "black"':
-            continue
-        if line.startswith("known_first_party ="):
-            line = line.replace(
-                "known_first_party",
-                "known-first-party",
-            )
-            updated_lines.append(line)
-            continue
-
-        updated_lines.append(line)
-
-    updated_lines.append("")
-    pyproject_toml.write_text("\n".join(updated_lines))
-
-
-def _remove_flake8_config():
-    flake8_config = Path(".flake8")
-    if flake8_config.exists():
-        flake8_config.unlink()
+    content = content.replace(
+        "relekang/python-semantic-release",
+        "python-semantic-release/python-semantic-release",
+    )
+    ci_yml.write_text(content)
 
 
 # You shouldn't need to change anything below this line
