8d840a3ddc7859db6e431a2aafbfafa6471a367c
Bruno Alla <alla.brunoo@gmail.com>
2023-10-17 20:11:01 +0100

    Fix PSR configs

--- run_fix.py
+++ run_fix.py
@@ -1,81 +1,36 @@
 from __future__ import annotations
 
 import argparse
+import re
 from pathlib import Path
 
 from all_repos import autofix_lib
 from all_repos.grep import repos_matching
 
 # Find repos that have this file...
-FILE_NAME = ".github/workflows/ci.yml"
+FILE_NAME = "pyproject.toml"
 # ... and which content contains this string.
-FILE_CONTAINS = "PYPI_TOKEN"
+FILE_CONTAINS = "version_variable = \""
 # Git stuff
-GIT_COMMIT_MSG = "ci: release to PyPI using Trusted Publisher"
-GIT_BRANCH_NAME = "ci/trusted-publisher"
+GIT_COMMIT_MSG = "chore: update PSR version_variables config"
+GIT_BRANCH_NAME = "chore/psr-version-variables"
 
-CONTENT = """\
-  release:
-    needs:
-      - test
-      - commitlint
-
-    runs-on: ubuntu-latest
-    environment: release
-    concurrency: release
-    permissions:
-      id-token: write
-      contents: write
-
-    steps:
-      - uses: actions/checkout@v4
-        with:
-          fetch-depth: 0
-          ref: ${{ github.head_ref || github.ref_name }}
-
-      # Do a dry run of PSR
-      - name: Test release
-        uses: python-semantic-release/python-semantic-release@v8.1.1
-        if: github.ref_name != 'main'
-        with:
-          root_options: --noop
-
-      # On main branch: actual PSR + upload to PyPI & GitHub
-      - name: Release
-        uses: python-semantic-release/python-semantic-release@v8.1.1
-        id: release
-        if: github.ref_name == 'main'
-        with:
-          github_token: ${{ secrets.GITHUB_TOKEN }}
-
-      - name: Publish package distributions to PyPI
-        uses: pypa/gh-action-pypi-publish@release/v1
-        if: steps.release.outputs.released == 'true'
-
-      - name: Publish package distributions to GitHub Releases
-        uses: python-semantic-release/upload-to-gh-release@main
-        if: steps.release.outputs.released == 'true'
-        with:
-          github_token: ${{ secrets.GITHUB_TOKEN }}
-"""
 
 
 def apply_fix():
     """Apply fix to a matching repo."""
-    ci_yml = Path(FILE_NAME)
-    content = ci_yml.read_text()
-    if "secrets.PYPI_TOKEN" not in content:
+    pyproject_toml = Path(FILE_NAME)
+    content = pyproject_toml.read_text()
+    if "version_variables = [" in content:
         return
 
-    new_lines = []
-    for line in content.splitlines():
-        if "  release:" in line:
-            break
-        new_lines.append(line)
+    content = re.sub(
+        r'version_variable = "([^"]+)"',
+        r'version_variables = [\n    "\1",\n]',
+        content,
+    )
 
-    new_lines.extend(CONTENT.splitlines())
-
-    ci_yml.write_text("\n".join(new_lines))
+    pyproject_toml.write_text(content)
 
 
 # You shouldn't need to change anything below this line
