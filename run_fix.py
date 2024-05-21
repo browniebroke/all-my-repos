1e5ab7b1d30b46aefaf6cd2cb87b1d460a14d0dc
Bruno Alla <alla.brunoo@gmail.com>
2024-05-21 20:27:49 +0100

    Upgrade Ruff settings

--- run_fix.py
+++ run_fix.py
@@ -9,43 +9,44 @@ from all_repos.grep import repos_matching
 # Find repos that have this file...
 FILE_NAME = "pyproject.toml"
 # ... and which content contains this string.
-FILE_CONTAINS = "tool.semantic_release"
+FILE_CONTAINS = "tool.ruff"
 # Git stuff
-GIT_COMMIT_MSG = "chore: add PSR branch groups to test releases on feature branches"
-GIT_BRANCH_NAME = "chore/psr-branch-groups"
-
-LINES_TO_INSERT = """[tool.semantic_release.branches.main]
-match = "main"
-
-[tool.semantic_release.branches.noop]
-match = "(?!main$)"
-prerelease = true
-
-"""
+GIT_COMMIT_MSG = "chore: upgrade ruff settings"
+GIT_BRANCH_NAME = "chore/upgrade-ruff-settings"
 
 
 def apply_fix():
     """Apply fix to a matching repo."""
     pyproject_toml = Path("pyproject.toml")
     file_content = pyproject_toml.read_text()
-    if "tool.semantic_release.branches.main" in file_content:
+    if "[tool.ruff.lint]" in file_content:
         return
 
-    breakpoint()
     updated_lines = []
-    inside_psr = False
-    branch_line_removed = False
+    inside_ruff = False
+    inside_lint_section = False
     for line in file_content.splitlines():
-        if line == "[tool.semantic_release]":
-            inside_psr = True
+        if line == "[tool.ruff]":
+            inside_ruff = True
 
-        if inside_psr and not branch_line_removed and line == 'branch = "main"':
-            branch_line_removed = True
+        if inside_ruff and not inside_lint_section and line.startswith("ignore = ["):
+            inside_lint_section = True
+            updated_lines.append("")
+            updated_lines.append("[tool.ruff.lint]")
+            updated_lines.append(line)
             continue
 
-        if line == "[tool.pytest.ini_options]":
-            updated_lines.extend(LINES_TO_INSERT.splitlines())
-            inside_psr = False
+        if inside_lint_section and line == "[tool.ruff.per-file-ignores]":
+            if updated_lines[-1] != "":
+                updated_lines.append("")
+            updated_lines.append("[tool.ruff.lint.per-file-ignores]")
+            continue
+
+        if inside_lint_section and line == "[tool.ruff.isort]":
+            if updated_lines[-1] != "":
+                updated_lines.append("")
+            updated_lines.append("[tool.ruff.lint.isort]")
+            continue
 
         updated_lines.append(line)
 
