76f3b7e5b73e8b985cddcb085934a159aefab7a9
Bruno Alla <alla.brunoo@gmail.com>
2026-04-18 20:11:04 -0300

    chore: mypy to ty

--- run_fix.py
+++ run_fix.py
@@ -9,87 +9,77 @@ from all_repos.grep import repos_matching
 # Find repos that have this file...
 FILE_NAMES = [".pre-commit-config.yaml"]
 # ... and which content contains this string.
-FILE_CONTAINS = "pre-commit.ci"
+FILE_CONTAINS = "pre-commit/mirrors-mypy"
 # Git stuff
-GIT_COMMIT_MSG = "ci: migrate from pre-commit to prek"
-GIT_BRANCH_NAME = "ci/pre-commmit-to-prek"
+GIT_COMMIT_MSG = "chore: migrate from mypy to ty"
+GIT_BRANCH_NAME = "chore/mypy-to-ty"
 
-JOB_TO_ADD = """  lint:
-    runs-on: ubuntu-latest
-    steps:
-      - uses: actions/checkout@v6.0.2
-      - uses: astral-sh/setup-uv@v8.0.0
-      - uses: j178/prek-action@v2.0.2
+
+PRE_COMMIT_TY = """
+  - repo: local
+    hooks:
+      - id: local-ty
+        name: ty check
+        entry: uv run ty check
+        require_serial: true
+        language: system
+        pass_filenames: false
 """
+
+PYPROJECT_CONFIG = """[tool.ty]
+terminal.error-on-warning = true
+
+""".splitlines()
 
 
 def apply_fix():
     """Apply fix to a matching repo."""
-    ci_yml = Path(".github/workflows/ci.yml")
-    if ci_yml.exists():
-        file_content = ci_yml.read_text()
-        if "j178/prek-action" not in file_content:
-            updated_lines = []
-            for line in file_content.splitlines():
+    # Update pre-commit config
+    pre_commit_config = Path(".pre-commit-config.yaml")
+    if pre_commit_config.exists():
+        file_content = pre_commit_config.read_text()
+        mypy_repo = False
+        updated_lines = []
+        for line in file_content.splitlines():
+            if "pre-commit/mirrors-mypy" in line:
+                mypy_repo = True
+                continue
+            if mypy_repo and line.startswith("  - repo: "):
+                mypy_repo = False
+            if not mypy_repo:
                 updated_lines.append(line)
-                if line == "jobs:":
-                    updated_lines.extend(JOB_TO_ADD.splitlines())
-                    updated_lines.append("")
-            file_content = "\n".join(updated_lines) + "\n"
-            file_content = file_content.replace(
-                "    needs:\n"
-                "      - test",
-                "    needs:\n"
-                "      - lint\n"
-                "      - test"
-            )
-            ci_yml.write_text(file_content)
+                continue
+        updated_content = "\n".join(updated_lines)
+        updated_content += PRE_COMMIT_TY
+        pre_commit_config.write_text(updated_content)
 
-    contributing_md = Path("CONTRIBUTING.md")
-    if contributing_md.exists():
-        file_content = contributing_md.read_text()
-        if "https://prek.j178.dev/" not in file_content:
-            file_content = file_content.replace(
-                "[pre-commit](https://pre-commit.com)",
-                "[prek](https://prek.j178.dev/)",
-            )
-            file_content = file_content.replace("pre-commit", "prek")
-            file_content = file_content.replace("prek install", "prek install -f")
-            contributing_md.write_text(file_content)
+    # Update pyproject.toml
+    pyproject_toml = Path("pyproject.toml")
+    file_content = pyproject_toml.read_text()
+    if "[tool.mypy]" in file_content:
+        updated_lines = []
+        mypy_section = False
+        for line in file_content.splitlines():
+            if line == "[tool.mypy]":
+                mypy_section = True
+                updated_lines.extend(PYPROJECT_CONFIG)
+                continue
+            if mypy_section and line.startswith("[tool."):
+                mypy_section = False
+            if not mypy_section:
+                updated_lines.append(line)
+                continue
 
-    readme_md = Path("README.md")
-    if readme_md.exists():
-        file_content = readme_md.read_text()
-        file_content = file_content.replace(
-            '<a href="https://github.com/pre-commit/pre-commit">',
-            '<a href="https://github.com/j178/prek">',
-        )
-        file_content = file_content.replace(
-            '<img src="https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit&logoColor=white&style=flat-square" alt="pre-commit">',
-            '<img src="https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/j178/prek/master/docs/assets/badge-v0.json" alt="prek">',
-        )
-        readme_md.write_text(file_content)
+        pyproject_toml.write_text("\n".join(updated_lines) + "\n")
 
-    gitpod_yaml = Path(".gitpod.yaml")
-    if gitpod_yaml.exists():
-        file_content = gitpod_yaml.read_text()
-        file_content = file_content.replace(
-            "      pip install pre-commit\n"
-            "      pre-commit install\n"
-            "      PIP_USER=false pre-commit install-hooks",
-            "      pip install prek\n"
-            "      prek install -f"
-        )
-        gitpod_yaml.write_text(file_content)
+    # Install ty
+    autofix_lib.run("uv", "add", "ty==0.0.31", "--dev")
 
-    pr_template_md = Path(".github/PULL_REQUEST_TEMPLATE.md")
-    if pr_template_md.exists():
-        file_content = pr_template_md.read_text()
-        file_content = file_content.replace(
-            '> - If pre-commit.ci is failing, try `pre-commit run -a` for further information.',
-            '> - If the lint job is failing, try `prek run -a` for further information.',
-        )
-        pr_template_md.write_text(file_content)
+    # Remove GitPod config
+    gitpod_yml = Path(".gitpod.yml")
+    if gitpod_yml.exists():
+        gitpod_yml.unlink(missing_ok=True)
+
 
 
 # You shouldn't need to change anything below this line
